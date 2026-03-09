66714599a9abff25b096fcc6988daffee3ebad18
patiltanmay8188-web <patiltanmay8188@gmail.com>
2026-03-10 01:54:07 +0530

    Create SimpleStorage.sol

---
+++ SimpleStorage.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+
+    int public value;
+
+    function increment() public {
+        value += 1;
+    }
+
+    function decrement() public {
+        value -= 1;
+    }
+
+    function getValue() public view returns(int){
+        return value;
+    }
+}
